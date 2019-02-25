e50b4c292715bd8978712e63b2ebc7a26a2fa5cf
Casey Blamires <clblamires@gmail.com>
2019-02-25 09:29:24 -0800

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  name: string;
+
+  constructor( 
+    public alertCtrl: AlertController, 
+    public toastCtrl: ToastController 
+  ){
+    this.getName();
+  }
+
+  async getName(){
+    let prompt = await this.alertCtrl.create({
+      header: 'Who are you?',
+      message: 'Please enter your name',
+      inputs: [{
+        name: 'username',
+        type: 'text'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel'
+      },{
+        text: 'Save',
+        handler: data => {
+          this.name = data.username;
+          toast.present();
+        }
+      }]
+    });
+
+    let toast = await this.toastCtrl.create({
+      message: "Name saved!",
+      duration: 5000,
+      showCloseButton: true,
+      color: 'dark'
+    });
+    
+
+
+    prompt.present();
+  }
+
+
+  ngOnInit(){}
 
 }
